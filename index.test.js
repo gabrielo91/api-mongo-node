a4e02f46a4520e8ad20089bc02ee7a365a5a1f43
Gabriel Ortega <gabrielo_9103@hotmail.com>
2020-04-09 23:53:14 -0500

    tests and dotenv added

--- index.test.js
+++ index.test.js
@@ -3,13 +3,9 @@ const sinon = require("sinon");
 const request = require("supertest");
 const IndexController = require("./controllers/IndexController");
 
-let IndexStub;
-
 describe("Test index API", () => {
   test("Index should return 200", async (done) => {
-    IndexStub = sinon
-      .stub(IndexController, "getTestResponse")
-      .returns({ hello: "test" });
+    sinon.stub(IndexController, "getTestResponse").returns({ hello: "test" });
     request(app).get("/test").expect(200, { hello: "test" }, done);
   });
 });
